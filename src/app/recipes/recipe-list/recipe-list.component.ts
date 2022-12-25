4d52775a8ef1faf486e617a46e44ac6d896695c7
emrebican <emrebican11@hotmail.com>
2022-12-25 19:58:48 +0300

    recipes and shopping list components created | In these components, some example items are added | models are created for both components | Project basics are done

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A test recipe',
+      'this is simply test',
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'this is simply test',
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
+    ),
+    new Recipe(
+      'A test recipe',
+      'this is simply test',
+      'https://ichef.bbci.co.uk/food/ic/food_16x9_832/recipes/british_shakshuka_26737_16x9.jpg'
+    )
+  ];
+
+  constructor() {}
+}
